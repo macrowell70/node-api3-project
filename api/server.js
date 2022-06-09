5c0f3552dcfdab91dcd238d055b39a3b688ba7c2
unknown <macrowell@yahoo.com>
2022-06-08 21:39:32 -0400

    set up endpoints

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = require('./users/users-router');
+const usersRouter = require('./users/users-router');
 
 const server = express();
 
@@ -9,6 +9,7 @@ server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
 
+server.use('/api/users', usersRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -9,36 +9,69 @@ const { logger, validateUserId, validateUser, validatePost } = require('../middl
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  Users.get()
+    .then(result => res.status(200).json(result))
+    .catch(err => res.json(err))
   // RETURN AN ARRAY WITH ALL THE USERS
 });
 
 router.get('/:id', (req, res) => {
+  const { id } = req.params
+  Users.getById(id)
+    .then(result => res.status(200).json(result))
+    .catch(err => res.json(err))
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
 });
 
 router.post('/', (req, res) => {
+  Users.insert(req.body)
+    .then(result => res.status(201).json(result))
+    .catch(err => res.json(err))
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
 });
 
 router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  Users.update(id, req.body)
+    .then(result => res.status(201).json(result))
+    .catch(err => res.json(err))
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
 router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  let post = ""
+  Users.getById(id)
+    .then(result => { return post = result })
+  Users.remove(id)
+    .then(result => res.status(200).json(post))
+    .catch(err => res.json(err))
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
 });
 
 router.get('/:id/posts', (req, res) => {
+  const { id } = req.params;
+  Posts.get()
+    .then(posts => {
+      const filteredPosts = posts.filter(post => post.user_id == id)
+      res.status(200).json(filteredPosts)
+    })
+    .catch(err => res.json(err))
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
 });
 
 router.post('/:id/posts', (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+  Posts.insert({ text: text, user_id: id })
+    .then(result => res.status(201).json(result))
+    .catch(err => res.json(err))
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
